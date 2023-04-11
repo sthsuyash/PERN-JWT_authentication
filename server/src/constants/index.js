87a32ee618ce8af9877683ea81d053ecfdda4ce2
Suyash Shrestha <sthasuyash11@gmail.com>
2023-04-11 19:22:11 +0545

    update constant for remote db

--- server/src/constants/index.js
+++ server/src/constants/index.js
@@ -6,7 +6,8 @@ exports.DB = {
     DB_NAME: process.env.DB_NAME,
     DB_USER: process.env.DB_USER,
     DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_HOST: process.env.DB_HOST
+    DB_HOST: process.env.DB_HOST,
+    DB_ENDPOINT_ID: process.env.DB_ENDPOINT_ID
 }
 
 exports.SERVER = {
